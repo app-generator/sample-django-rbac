09d9af79eb6e9044bce6972f2ab009019631e35a
mahfujur <mahfujul.hoque@heliosinsider.com>
2024-03-17 14:06:09 +0600

    add-routes

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('route1/', views.route1, name='route1'),
+    path('route2/', views.route2, name='route2'),
+
+    # Authentication
+    path('accounts/login/', views.UserLoginView.as_view(), name="login"),
+    path('accounts/register/', views.register, name="register"),
+    path('accounts/logout/', views.logout_view, name="logout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,14 +1,66 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from datetime import datetime
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test
 from .models import *
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from home.forms import LoginForm, RegistrationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/accounts/login/')
 def index(request):
 
-  context = {
-    'date': datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
-  }
-  return HttpResponse("Hello DJANGO! time is: " + context['date'])
+    return render(request, 'index.html')
+
+
+
+
+def is_route1(user):
+    return user.groups.filter(name='group1').exists()
+
+def is_route2(user):
+    return user.groups.filter(name='group2').exists()
+
+@user_passes_test(is_route1)
+def route1(request):
+   
+    return render(request, 'route1.html')
+
+@user_passes_test(is_route2)
+def route2(request):
+   
+    return render(request, 'route2.html')
+
+
+#authentication
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    form_class = LoginForm
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+def register(request):
+    if request.method == 'POST':
+        form =  RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print('account created successfully')
+            return redirect ('login')
+        else:
+            print('register failed')
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form
+    }               
+
+    return render(request, 'accounts/register.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
